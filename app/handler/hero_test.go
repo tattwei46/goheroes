0e0af802f5a91e4c1f37c5a9621cf5d3cab57f8c
DavidCheah <tattwei46@gmail.com>
2019-10-20 21:44:14 +0800

    Added http unit testing

---
+++ app/handler/hero_test.go
@@ -0,0 +1,61 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gorilla/mux"
+)
+
+func TestGetHeroes(t *testing.T) {
+	req, err := http.NewRequest("GET", "/heroes", nil)
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetHeroes)
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected status ok; got %v", rr.Code)
+	}
+
+	expected := `[{"id":"1","name":"Ironman"}]`
+	result := strings.TrimSpace(rr.Body.String())
+
+	if result != expected {
+		t.Errorf("handle returned unexpected body, got %v, want %v", result, expected)
+	}
+}
+
+func TestGetHeroById(t *testing.T) {
+	req, err := http.NewRequest("GET", "/hero", nil)
+
+	vars := map[string]string{
+		"id": "1",
+	}
+
+	req = mux.SetURLVars(req, vars)
+
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetHero)
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected status ok; got %v", rr.Code)
+	}
+
+	expected := `{"id":"1","name":"Ironman"}`
+	result := strings.TrimSpace(rr.Body.String())
+
+	if result != expected {
+		t.Errorf("handle returned unexpected body, got %v, want %v", result, expected)
+	}
+}
